75603b92f8b093e62723413ed815c54d71c61ad9
Christopher J. White <c.white.astro@gmail.com>
2019-05-19 02:35:26 -0700

    added basic main function and empty header

---
+++ src/ray_trace.cpp
@@ -0,0 +1,23 @@
+// Ray Trace main file
+
+// C++ headers
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+
+// Ray Trace headers
+#include "ray_trace.hpp"
+
+// Main function
+int main(int argc, char *argv[])
+{
+  // Parse inputs
+  if (argc != 2)
+  {
+    std::cout << "Error: Must give a single input file.\n";
+    return 1;
+  }
+
+  // End program
+  return 0;
+}
---
+++ src/ray_trace.hpp
@@ -0,0 +1,6 @@
+// Ray Trace header
+
+#ifndef RAY_TRACE_H_
+#define RAY_TRACE_H_
+
+#endif
