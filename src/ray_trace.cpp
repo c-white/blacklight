8de473fb4dab2effed674848afe6794a13393615
Christopher J. White <c.white.astro@gmail.com>
2019-05-19 12:57:04 -0700

    added code to find length of input file

--- src/ray_trace.cpp
+++ src/ray_trace.cpp
@@ -1,9 +1,8 @@
 // Ray Trace main file
 
 // C++ headers
-#include <cstdlib>
-#include <iostream>
-#include <limits>
+#include <filesystem>  // filesystem
+#include <iostream>    // cout
 
 // Ray Trace headers
 #include "ray_trace.hpp"
@@ -11,12 +10,17 @@
 // Main function
 int main(int argc, char *argv[])
 {
-  // Parse inputs
+  // Parse command line inputs
   if (argc != 2)
   {
     std::cout << "Error: Must give a single input file.\n";
     return 1;
   }
+  const char *input_file = argv[1];
+
+  // Read input file
+  unsigned long int input_file_length = std::filesystem::file_size(input_file);
+  std::cout << input_file_length << "\n";
 
   // End program
   return 0;
