74d4a533b53b960584d6a1dcb7fafb58bd4932e9
Christopher J. White <c.white.astro@gmail.com>
2019-05-21 00:34:07 -0700

    added multidimensional arrays

---
+++ src/array.cpp
@@ -0,0 +1,96 @@
+// Ray Trace multidimensional array
+
+// Ray Trace headers
+#include "array.hpp"
+#include "exceptions.hpp"  // ray_trace_exception
+
+// Instantiations
+template struct array<int>;
+
+//--------------------------------------------------------------------------------------------------
+
+// Multidimensional array constructor (empty)
+// Inputs: (none)
+template<typename type>
+array<type>::array()
+{
+  allocated = false;
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Multidimensional array constructor (1D)
+// Inputs: (none)
+template<typename type>
+array<type>::array(int n1_)
+  : n1(n1_)
+{
+  allocate();
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Multidimensional array destructor
+template<typename type>
+array<type>::~array()
+{
+  if (allocated)
+    delete[] data;
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Multidimensional array allocator (general)
+// Inputs: (none)
+// Outputs: (none)
+template<typename type>
+void array<type>::allocate()
+{
+  if (allocated)
+    throw ray_trace_exception("Error: Attempting to reallocate array.");
+  if (n1 == 0)
+    throw ray_trace_exception("Error: Attempting to allocate empty array.");
+  else
+    data = new type[n1];
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Multidimensional array allocator (1D)
+// Inputs:
+//   n1_: number of elements
+// Outputs: (none)
+template<typename type>
+void array<type>::allocate(int n1_)
+{
+  if (allocated)
+    throw ray_trace_exception("Error: Attempting to reallocate array.");
+  n1 = n1_;
+  data = new type[n1];
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Multidimensional array read accessor (1D)
+// Inputs:
+//   n: index
+// Outputs:
+//   returned value: element
+template<typename type>
+type array<type>::operator()(int n) const
+{
+  return data[n];
+}
+
+//--------------------------------------------------------------------------------------------------
+
+// Multidimensional array read/write accessor (1D)
+// Inputs:
+//   n: index
+// Outputs:
+//   returned value: reference to element
+template<typename type>
+type &array<type>::operator()(int n)
+{
+  return data[n];
+}
---
+++ src/array.hpp
@@ -0,0 +1,31 @@
+// Ray Trace multidimensional array header
+
+#ifndef ARRAY_H_
+#define ARRAY_H_
+
+//--------------------------------------------------------------------------------------------------
+
+// Multidimensional array
+template<typename type>
+struct array
+{
+  // Constructors and destructor
+  array();
+  array(const array<type> &source) = delete;
+  array &operator=(const array<type> &source) = delete;
+  array(int n1_);
+  ~array();
+
+  // Data
+  type *data;
+  int n1;
+  bool allocated;
+
+  // Functions
+  void allocate();
+  void allocate(int n);
+  type operator()(int n) const;
+  type &operator()(int n);
+};
+
+#endif
